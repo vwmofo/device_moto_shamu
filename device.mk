00ce3a4677230045f8c1ec2071726d06d23201e3
dhacker29 <dhackerdvm@gmail.com>
2014-12-21 00:43:42 -0500

    shamu: Add nfcee_access.xml
    Change-Id: I3dab3147b533e9845b5910cdfab1c21c435f1437

--- device.mk
+++ device.mk
@@ -370,6 +370,10 @@ PRODUCT_PACKAGES += \
     Tag \
     android.hardware.nfc@1.0-impl
 
+# NFCEE access control
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/nfcee_access.xml:system/etc/nfcee_access.xml
+
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.nfc.xml:system/etc/permissions/android.hardware.nfc.xml \
     frameworks/native/data/etc/android.hardware.nfc.hce.xml:system/etc/permissions/android.hardware.nfc.hce.xml \
