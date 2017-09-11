388507d2a794bb34dbd74d766725169a5b6ec515
vwmofo <kkozma@gmail.com>
2017-09-11 19:05:05 -0400

    NFC:  Add BOARD_NFC_HAL_MODULE
    To configure the interface to allow our old broadcom hal.
    
    Requires:  https://github.com/vwmofo/platform_hardware_libhardware/commit/28f313c7ece7d350fc5abb0f1e6fd4534cdcf73f

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -53,6 +53,9 @@ BOARD_EGL_CFG := device/moto/shamu/egl.cfg
 
 BOARD_USES_ALSA_AUDIO := true
 
+# NFC
+BOARD_NFC_HAL_MODULE := BCM2079X
+
 # Wifi related defines
 WPA_SUPPLICANT_VERSION      := VER_0_8_X
 BOARD_WLAN_DEVICE           := bcmdhd
